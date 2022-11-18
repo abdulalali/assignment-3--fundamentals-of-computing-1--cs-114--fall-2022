3e70340ec2e86b52536250360e6939bda480d549
abdulalali <113354295+abdulalali@users.noreply.github.com>
2022-11-18 16:24:27 -0500

    Assignment 3

--- Matrix.java
+++ Matrix.java
@@ -1,3 +1,62 @@
-public class Matrix {
+import java.util.Scanner;
 
+
+public class Matrix {
+    private int [][]matrix;
+    
+    public Matrix(int size){
+        matrix = new int[size][size];
+        System.out.println("Created matrix, size = "+size);
+    }
+    
+    private void swap(int x1, int y1, int x2, int y2){
+        int temp = matrix[x1][y1];
+        matrix[x1][y1] = matrix[x2][y2];
+        matrix[x2][y2] = temp;
+    }
+    
+    public void printMatrix(){
+        int i;
+        int j;
+        int x1 = 0;
+        int y1 = matrix.length - 1;
+        System.out.println();
+        for(i=0; i<matrix.length; i++){
+            for(j=0; j<matrix.length; j++){
+                if(i == x1 && j == y1){
+                    System.out.print("\u001B[33m");
+                }
+                System.out.printf("%4d ", matrix[i][j]);
+                System.out.print("\u001B[0m");
+            }
+            x1++;
+            y1--;
+            System.out.println();
+        }
+        System.out.println();
+    }
+    
+    public void populateMatrix(){
+        Scanner in = new Scanner(System.in);
+        int i;
+        int j;
+        System.out.println("Enter matrix values: ");
+        for(i=0; i<matrix.length; i++){
+            for(j=0; j<matrix.length; j++){
+                matrix[i][j] = in.nextInt();
+            }
+        }
+    }
+    public void flipMatrix(){
+        int i;
+        int j;
+        int n = matrix.length;
+        for(i=0; i<n; i++){
+            for(j=0; j < n - 1 - i; j++){
+                swap(i, j, n-1-j, n-1-i);
+            }
+        }
+            
+    }
 }
+
--- RunMatrix.java
+++ RunMatrix.java
@@ -1,5 +1,11 @@
-import java.util.Scanner;
 
 public class RunMatrix {
-
+    public static void main(String[] args){
+        Matrix m = new Matrix(5);
+        m.populateMatrix();
+        m.printMatrix();
+        m.flipMatrix();
+        m.printMatrix();
+    }
 }
+
